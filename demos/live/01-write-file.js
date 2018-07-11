b65c2f67b0cdddbd689d5d19ff68a6a59d6bf6d7
Dean Radcliffe <deanmisc@yahoo.com>
2018-07-11 11:21:09 -0500

    docs: Live Demo 1.2 - Define and process an action; no results yet.

--- demos/live/01-write-file.js
+++ demos/live/01-write-file.js
@@ -1,3 +1,13 @@
-const { Agent } = require('antares-protocol')
+const { Agent } = require("antares-protocol")
 const agent = new Agent()
 
+const writeLine = text => ({
+  payload: {
+    text,
+    encoding: "UTF8",
+    path: "../scratch/live-actors.yml"
+  }
+})
+
+const action = writeLine('Jake Weary')
+agent.process(action)
