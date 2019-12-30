949b4f08474982998f36a656bf69e38947bdaf80
Dean Radcliffe <deanmisc@yahoo.com>
2019-12-30 15:03:06 -0600

    NOJIRA: WIP

--- test/trivia.integration.test.ts
+++ test/trivia.integration.test.ts
@@ -4,7 +4,7 @@ import { init as initStore } from "@rematch/core"
 import { triviaStoreConfig } from "../demos/trivia/store"
 
 describe("Multi-agent Trivia Game", () => {
-  let moderator: StoreAgent // the server
+  let server: StoreAgent // the server
   let player1: StoreAgent // a player
   let emcee: StoreAgent // the real-life game operator
   const pantsQ = { prompt: "Pants?", choices: ["Yes", "No"] }
@@ -12,31 +12,32 @@ describe("Multi-agent Trivia Game", () => {
 
   it.only("should play the whole game without error", () => {
     // set up questions
-    player1.process({ type: "game/addQuestion", payload: pantsQ })
-    expect(moderator.state.game).toMatchObject({
+    player1.trigger("game/addQuestion", pantsQ)
+    expect(server.state.game).toMatchObject({
       questions: [pantsQ]
     })
-    // players join
-    player1.process({ type: "game/joinPlayer", payload: { name: "Declan" }, meta: { push: true } })
+
+    // Players joining (on their local device) are known by the server
+    player1.trigger("game/joinPlayer", { name: "Declan" })
     const joinedState = {
       players: [{ name: "Declan" }]
     }
-    expect(moderator.state.game).toMatchObject(joinedState)
-    expect(moderator.state.game).toMatchObject(joinedState)
+    expect(server.state.game).toMatchObject(joinedState)
+    expect(server.state.game).toMatchObject(joinedState)
 
     // begin game
     emcee.process({ type: "game/nextQuestion", payload: pantsQ, meta: { push: true } })
-    const roundUnanswered = (hideAnswers = false) => ({
+    const roundUnanswered = {
       game: {
         status: "playing",
         questions: []
       },
       round: {
-        question: Object.assign(pantsQ, hideAnswers ? {} : pantsA)
+        question: Object.assign(pantsQ, pantsA)
       }
-    })
-    expect(moderator.state).toMatchObject(roundUnanswered())
-    expect(player1.state).toMatchObject(roundUnanswered())
+    }
+    expect(server.state).toMatchObject(roundUnanswered)
+    expect(player1.state).toMatchObject(roundUnanswered)
     // players answer when the round changes
 
     const playerAnswer = { from: player1.agentId, choice: "No" }
@@ -46,8 +47,8 @@ describe("Multi-agent Trivia Game", () => {
     })
 
     expect(player1.state.round.responses).toContain(playerAnswer)
-    expect(moderator.state.round.responses).toContain(playerAnswer)
-    expect(moderator.state.round.summary).toMatchObject({ Yes: 0, No: 1 })
+    expect(server.state.round.responses).toContain(playerAnswer)
+    expect(server.state.round.summary).toMatchObject({ Yes: 0, No: 1 })
 
     // Once the next question is chosen by the emcee, saveResponses
     // will process just prior to nextQuestion
@@ -66,49 +67,27 @@ describe("Multi-agent Trivia Game", () => {
   })
 
   beforeAll(() => {
-    moderator = new StoreAgent(initStore(triviaStoreConfig), { agentId: "moderator" })
+    server = new StoreAgent(initStore(triviaStoreConfig), { agentId: "server" })
     player1 = new StoreAgent(initStore(triviaStoreConfig), { agentId: "player1" })
     emcee = new StoreAgent(initStore(triviaStoreConfig), { agentId: "emcee" })
 
     // The topology defines who sends events to whom
     const topology = {
-      player1: [moderator],
-      emcee: [moderator],
-      moderator: [player1, emcee]
+      player1: [server],
+      emcee: [server],
+      server: [player1, emcee]
     }
+    const players = [player1, emcee]
 
-    // Set up agents' properties and relationships to each other
-    ;[moderator, player1, emcee].forEach(agent => {
-      // Stamp events with agentId
-      agent.filter(true, ({ event }) => {
-        event.payload.agentId = agent.agentId
+    // Follow the topology.. Each player to the servers, the server to players, emcee
+    players.forEach(player => {
+      player.filter(["game/addQuestion", "game/joinPlayer", "game/nextQuestion"], ({ event }) => {
+        event.relay !== false && server.process(event)
       })
+    })
 
-      // Agents send events to the others in their topology
-      const others = topology[agent.agentId]
-      agent.filter(
-        () => true,
-        ({ event }) => {
-          const meta = event.meta || {}
-
-          // events marked meta.push false are not even sent to the server
-          if (meta["push"] === false) return
-
-          // We send out events to others but tell them not to push them
-          others.forEach(targetAgent => {
-            targetAgent.process({
-              ...event,
-              meta: {
-                ...(event.meta || {}),
-                // Except, we can tell the moderator to push to all others
-                // This will change once diffs of the store are filtered and
-                // pushed, instead of the events that caused them.
-                push: targetAgent.agentId === "moderator" ? true : false
-              }
-            })
-          })
-        }
-      )
+    server.filter(["game/nextQuestion"], ({ event }) => {
+      players.forEach(player => player.process({ ...event, relay: false }))
     })
   })
 })
